c7ab93fd32bafbc4f152f40a193303c6a858c68b
hal_neko <35726160+halneko5121@users.noreply.github.com>
2019-06-07 09:51:48 +0000

    add リロード、ブラウザバック対策を一旦入れた

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
 
   post "/", to: "toppages#index"
+
   post "search", to: "toppages#search"
+  get "search", to: "toppages#index"
+
   post "comparison", to: "toppages#comparison"
+  get "comparison", to: "toppages#index"
 end
