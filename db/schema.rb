8140fbd1717edee034af7f1b4a586b04f863d87b
hal_neko <35726160+halneko5121@users.noreply.github.com>
2019-06-09 08:25:43 +0000

    add databases/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_045807) do
+ActiveRecord::Schema.define(version: 2019_06_09_082252) do
 
   create_table "comparison_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2019_06_05_045807) do
     t.string "sales_date"
     t.string "isbn_code"
     t.string "jan_code"
+  end
+
+  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
