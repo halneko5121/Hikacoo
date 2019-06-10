9d4e8e69c2b751feabaa4bca4814fb4ff6107351
hal_neko <35726160+halneko5121@users.noreply.github.com>
2019-06-10 14:16:16 +0000

    clean ジャンル -> カテゴリー修正中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_082252) do
+ActiveRecord::Schema.define(version: 2019_06_10_141520) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comparison_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
